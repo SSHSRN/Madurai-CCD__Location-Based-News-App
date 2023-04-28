755a221e3ce4099df5d0a528110aac2049b82ad7
Srihari S <sshsrn@gmail.com>
2023-04-28 16:07:47 +0530

    feat: added frontend views
    Signed-off-by: Srihari S <sshsrn@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function sendAPIReq() {
+  const link = document.querySelector(".linkInput").value;
+  if (link === "") {
+    alert("Please enter a valid YouTube video URL, da!");
+    return;
+  }
+  if(link.startsWith("https://www.youtube.com/watch?v=")){
+    console.log("The link is valid");
+    console.log(link);
+  }
+  else{
+    alert("Please enter a valid YouTube video URL, da!");
+    return;
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="AppHeader allCenter">
+        <h1>YouTube Video Transcript Generator</h1>
+      </div>
+      <div className="allCenter hFull">
+        <p className="aboutText">
+          This is a simple web app that generates a <strong>transcript</strong> of a <strong>YouTube video</strong>. It uses the <strong>Whisper</strong> API and a combination of <strong>python</strong> and <strong>node.js</strong> to generate the transcript.<br />
+          To use this app, simply enter the URL of the YouTube video you want to generate the transcript for, and click the <strong>Generate Transcript</strong> button.<br />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input className="linkInput mt-3" type="text" placeholder="Enter YouTube video URL(https://www.youtube.com/watch?v=...)" />
+        <button className="mt-5 submitBtn" onClick={sendAPIReq}>Generate Transcript</button>
+      </div>
     </div>
   );
 }
